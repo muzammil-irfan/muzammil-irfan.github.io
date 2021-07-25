3e59ec9de587a4778a574a3379eaff6b9cd1577e
muzammil-irfan <79828942+muzammil-irfan@users.noreply.github.com>
2021-07-25 17:09:01 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+//question 4
+function MyCons(name, gender, address, education, profession){
+    this.name = name;
+    this.gender = gender;
+    this.address = address; 
+    this.education = education;
+    this.profession = profession;
+    
+};
+
+
+function submit(){
+    var name = document.getElementById("name").value;
+    var gen = document.getElementsByClassName("gen").value;
+    var add = document.getElementById("add").value;
+    var edu = document.getElementById("edu").value;
+    var pro = document.getElementById("pro").value;
+    var total = MyCons(name,gen,add,edu,pro);
+    document.write(total);
+}
+//question 1
 var itemsArray = [
     {name: "juice",price: 50, quantity: 3},
     {name: "cookie",price: 30, quantity: 9},
@@ -18,6 +39,7 @@ var obj ={
     city:"karachi",
     country:"Pak"
 };
+//question2
 var ageCheck = "age" in obj;
 document.write("<br><br> Age exist in object or not? <br>" + ageCheck);
 var countryCheck = "country" in obj;
@@ -26,7 +48,7 @@ var firstCheck = "FirstName" in obj;
 document.write("<br><br> FirstName exist in object or not? <br>" + firstCheck);
 var lastCheck = "LastName" in obj;
 document.write("<br><br> LastName exist in object or not? <br>" + lastCheck);
-
+//question 3
 function Plan(name,age,email){
     this.name = name;
     this.age = age;
@@ -36,21 +58,3 @@ var plan1 = new Plan("basit",15,"basit@gmail.com");
 var plan2 = new Plan("khubaib",16,"khubaib@gmail.com");
 document.write("<br>" + plan1.name + "<br>");
 document.write(plan2.name + "<br>");
-//question 4
-function MyCons(name, gender, address, education, profession){
-    this.name = name;
-    this.gender = gender;
-    this.address = address; 
-    this.education = education;
-    this.profession = profession;
-};
-var name = document.getElementById("name").value;
-var gen = document.getElementsByClassName("gen").value;
-var add = document.getElementById("add").value;
-var edu = document.getElementById("edu").value;
-var pro = document.getElementById("pro").value;
-
-function submit(){
-  var total = MyCons(name,gen,add,edu,pro);
-    document.write(name);
-}
